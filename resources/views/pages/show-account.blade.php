facc0113fbfa4beb93ff759add8b580d779fa3fb
RenataNorkiene <renata.norkiene@gmail.com>
2021-07-26 11:43:52 +0300

    added table to create account page

--- resources/views/pages/show-account.blade.php
+++ resources/views/pages/show-account.blade.php
@@ -1,48 +1,52 @@
 @extends('layouts.app')
 @auth()
 @section('content')
-    <div class="p-3 mb-2 bg-primary text-white"> <h3>Transactions table</h3></div>
-    <table class="table table-bordered bg-white">
-        <thead>
-        <tr>
-            <th scope="col">Data</th>
-            <th scope="col">Account from</th>
-            <th scope="col">Account to</th>
-            <th scope="col">Full Name</th>
-            <th scope="col">Description</th>
-            <th scope="col">Amount</th>
-            <th scope="col">Status</th>
-        </tr>
-        </thead>
-        <tbody>
-        @if(count($transfers) > 0)
-            @foreach($transfers as $transfer)
-
+<ul class="list-group">
+    <li class="list-group-item bg-primary text-white"><h3 class="text-center">Transfers table</h3></li>
+        <div class="table-responsive">
+            <table class="table table-bordered bg-white text-center">
+                <thead>
                 <tr>
-                    <td>{{$transfer->created_at}}</td>
-                    <td>{{$transfer->account_number_from}}</td>
-                    <td>{{$transfer->account_number_to}}</td>
-                    <td>{{$transfer->full_name}}</td>
-                    <td>{{$transfer->description}}</td>
-                    @if($account_number == $transfer->account_number_from)
-                        <td>-{{$transfer->amount}}</td>
-                    @else
-                        <td>+{{$transfer->amount}}</td>
-                    @endif
-                    <td>
-                    @if(time() - strtotime($transfer->created_at) < 120)
-                        <button type="button" class="btn btn-danger">Cancel Transfer</button>
-                    @else
-                        Completed
-                    @endif
-                    </td>
+                    <th scope="col">Data</th>
+                    <th scope="col">Amount from</th>
+                    <th scope="col">Amount to</th>
+                    <th scope="col">Full name</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Amount</th>
+                    <th scope="col">Status</th>
                 </tr>
-        </tbody>
-        @endforeach
-        @else
-            Transactions not found
-        @endif
-    </table>
+                </thead>
+                <tbody>
+                <tr>
+                    @if(count($transfers) > 0)
+                        @foreach($transfers as $transfer)
+                            <td>{{$transfer->created_at}}</td>
+                            <td>{{$transfer->account_number_from}}</td>
+                            <td>{{$transfer->account_number_to}}</td>
+                            <td>{{$transfer->full_name}}</td>
+                            <td>{{$transfer->description}}</td>
+                            @if($account_number == $transfer->account_number_from)
+                                <td>-{{$transfer->amount}}</td>
+                            @else
+                                <td>+{{$transfer->amount}}</td>
+                            @endif
+                           <td>
+                               @if(time() - strtotime($transfer->created_at) < 120)
+                                   <button type="button" class="btn btn-danger">Cancel Transfer</button>
+                               @else
+                                   Completed
+                               @endif
+                           </td>
+                    </tr>
+                @endforeach
+                </tbody>
+                @else
+                    Transactions not found
+                @endif
+            </table>
+        </div>
+</ul>
 @endsection
 @endauth
+
 
